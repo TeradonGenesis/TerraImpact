95ec3e1bb55befd9294250f08a7c37703b50bac4
TeradonGenesis <48543482+TeradonGenesis@users.noreply.github.com>
2019-12-08 02:00:26 +0800

    dded backup database and added limit to tweet up to 280 limit

--- TerraImpact/app/models/impact.rb
+++ TerraImpact/app/models/impact.rb
@@ -1,7 +1,7 @@
 class Impact < ApplicationRecord
     include Friendlyable
     
-    validates :impact, presence: :true
+    validates :impact, presence: :true, length: { maximum: 280 }
     validates :category, presence: :true, uniqueness: {case_sensitive: false}
     
     belongs_to :user
